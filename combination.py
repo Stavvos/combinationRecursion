39bcaa2be23a151665f124a79e66c9d57030d5fd
Liam Henry <liam_burridgeuc@hotmail.com>
2021-11-20 20:14:16 +1100

    Added comments, and fixed returning decimal answer issue

--- combination.py
+++ combination.py
@@ -1,12 +1,19 @@
-def combination(n,r):
-	c = factorial(n) / (factorial (r) * factorial(n - r))
+#Written by liam henry 20/11/2021
+
+def combination(n,r): #combination function
+	c = factorial(n) // (factorial (r) * factorial(n - r)) #double // to avoid decimal answers
 	return c
 
-def factorial (x):
-	if x == 1:
-		return x
-	else:
+def factorial (x): #factorial function
+	if x == 1: #base case
+		return x 
+	else:	   #recursive case
 		return x * factorial(x - 1)
 
 
 print (combination(6,2))
+
+#c = n!/(r!(n-r)!)
+#c = combination
+#n = number of items in set
+#r = number of choosing objects from the set
